09594e5d8e0f4b58fc212d83c78a58eac364b6f2
alaeddinehamroun <71340201+alaeddinehamroun@users.noreply.github.com>
2022-06-04 21:01:03 +0100

    Committing files

---
+++ CodeForces/GNU C++17/709A | Juicer/159153274.cpp
@@ -0,0 +1,51 @@
+/********   All Required Header Files ********/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+ 
+using namespace std;
+ 
+int main() {
+    int n, b, d;
+    cin >> n >> b >> d;
+    int number, result = 0, total = 0;
+    while (cin >> number) {
+ 
+ 
+        if ( number <= b) {
+            total += number;
+            if (total > d) {
+                result ++;
+                total = 0;
+            }
+ 
+        };
+ 
+    }
+ 
+    cout << result;
+    return 0;
+ 
+}
+ 
+ 
