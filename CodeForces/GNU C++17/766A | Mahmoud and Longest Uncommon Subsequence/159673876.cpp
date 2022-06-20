e200346abd79a8a62ead7cedf48870e76fd516f7
alaeddinehamroun <71340201+alaeddinehamroun@users.noreply.github.com>
2022-06-20 21:23:46 +0100

    Committing files

---
+++ CodeForces/GNU C++17/766A | Mahmoud and Longest Uncommon Subsequence/159673876.cpp
@@ -0,0 +1,41 @@
+/********   All Required Header Files ********/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    string a, b;
+    cin >> a >> b;
+    if ( a == b)
+        cout << -1;
+    else
+        cout << max(a.length(), b.length());
+ 
+ 
+ 
+    return 0;
+}
+ 
